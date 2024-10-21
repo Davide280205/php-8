c45ee2b32485869a6744d0ee6e104141eea502d4
Davide <corpus.davide@gmail.com>
2024-10-21 09:56:59 +0200

    footer date

--- includes/step5/includes/footer.php
+++ includes/step5/includes/footer.php
@@ -1,3 +1,5 @@
+<!-- per aggiungere la data in PHP basta stampare date e aggiungere D/M/Y -->
+
 <footer>
-    <p>&copy; 2006&ndash;2021 David Powers</p>
+    <p>&copy; 2006 &ndash; <?php echo date('D/M/Y') ?> Davide Corpus</p>
 </footer>
