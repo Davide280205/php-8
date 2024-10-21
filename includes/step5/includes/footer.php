a6f13d09e7e9b09ea724b0597e0a53427ec574da
Davide <corpus.davide@gmail.com>
2024-10-21 10:14:25 +0200

    footer step5

--- includes/step5/includes/footer.php
+++ includes/step5/includes/footer.php
@@ -1,5 +1,26 @@
 <!-- per aggiungere la data in PHP basta stampare date e aggiungere D/M/Y -->
 
 <footer>
-    <p>&copy; 2006 &ndash; <?php echo date('D/M/Y') ?> Davide Corpus</p>
+
+    <?php
+
+        $startYear = 2006;
+        $thisYear = date('Y');
+
+        if ($startYear == $thisYear) {
+            
+            $output = $startYear;
+
+        } else {
+
+            $output = "{$startYear}&ndash;{$thisYear}";
+
+        }
+
+    ?>
+
+    <?php // il <? (short echo tag) è una maniera più breve per scrivere il tag PHP ?>
+
+    <p>&copy; <?= $output ?> Davide Corpus</p>
+
 </footer>
