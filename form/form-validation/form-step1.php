74b2c4dea469e30f5ddd430ea1659422f522209f
Davide <corpus.davide@gmail.com>
2024-10-23 10:06:25 +0200

    form-step1 - function

--- form/form-validation/form-step1.php
+++ form/form-validation/form-step1.php
@@ -18,6 +18,20 @@
   if ($_SERVER["REQUEST_METHOD"]== "POST") {
 
     $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $website = test_input($_POST["website"]);
+    $comment = test_input($_POST["comment"]);
+    $gender = test_input($_POST["gender"]);
+
+  }
+
+  function test_input($data) {
+
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
 
   }
 
