bbf68ff3a5752cbf5fcf92105997f781c67d6efa
Davide <corpus.davide@gmail.com>
2024-10-24 15:52:58 +0200

    classes - Page_Data

---
+++ portfolio-site/step6/classes/Page_Data.class.php
@@ -0,0 +1,10 @@
+<?php
+
+    class Page_Data {
+
+        
+
+    }
+    
+
+?>
--- portfolio-site/step6/templates/page.php
+++ portfolio-site/step6/templates/page.php
@@ -7,6 +7,8 @@
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>$pageData->title</title>
+        $pageData->css
+        $pageData->embeddedStyle
     </head>
     <body>
 
