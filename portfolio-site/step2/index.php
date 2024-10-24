2916baf5a2a6a908dafc47e60d52fc0c53ba9080
Davide <corpus.davide@gmail.com>
2024-10-24 14:09:52 +0200

    index

--- portfolio-site/step2/index.php
+++ portfolio-site/step2/index.php
@@ -3,7 +3,7 @@
     include_once "views/navigation.php";
     $pageData = new stdClass();
     $pageData->title = "Porfolio test-site";
-    $pageData->content = "<h1>Hello from an objects</h1>";
+    $pageData->content = $nav;
 
     require "templates/page.php";
     echo $page;
