9fe5e85f83fd3bf8f172bb6bf21aacc1454d6d18
Davide <corpus.davide@gmail.com>
2024-10-24 15:28:43 +0200

    step5

---
+++ portfolio-site/step5/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    // stampa i link di navigation.php
+
+    include_once "views/navigation.php";
+    $pageData = new stdClass();
+    $pageData->title = "Davide Corpus Porfolio";
+    // mette la navigazione nel contenuto
+    $pageData->content = $nav;
+
+    // aggiunta
+
+    // isset controlla se una variabile ESISTE
+
+    $navigationIsClicked = isset($_GET['page']);
+
+    if ($navigationIsClicked) {
+
+        $fileToLoad = $_GET['page'];
+        include_once "views/$fileToLoad.php";
+        $pageData->content .= $info;
+
+    }
+
+    require "templates/page.php";
+    echo $page;
+
+?>
