c7b1c47ea67124d4214f59d68878fcc263bb838c
Davide <corpus.davide@gmail.com>
2024-10-24 14:03:22 +0200

    navigation

---
+++ portfolio-site/step2/views/navigation.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nav = "
+    
+    <nav>
+
+        <a href='index.php?page=skills'>Le mie competenze</a>
+        <a href='index.php?page=projects'>I miei progetti</a>
+
+    </nav>
+    
+    
+    
+    ";
+
+?>
