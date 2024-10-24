4a640a4b0ab02f0c70300e1347a8a341dff16147
Davide <corpus.davide@gmail.com>
2024-10-24 15:42:01 +0200

    step6

---
+++ portfolio-site/step6/views/navigation.php
@@ -0,0 +1,16 @@
+<?php
+
+    // gli href sono degli URL VARIABILI
+
+    $nav = "
+    
+    <nav>
+
+        <a href='index.php?page=skills'>Le mie competenze</a>
+        <a href='index.php?page=projects'>I miei progetti</a>
+
+    </nav>   
+    
+    ";
+
+?>
---
+++ portfolio-site/step6/views/skills.php
@@ -0,0 +1,7 @@
+<?php
+
+    $info = "<h1>Le mie competenze</h1>
+    <p>Leggi tutte le mie competenze...</p>     
+    ";
+
+?> 
