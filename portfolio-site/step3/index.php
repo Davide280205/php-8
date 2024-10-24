c088358f635afafc0e21d2e0276e6e8fea33254c
Davide <corpus.davide@gmail.com>
2024-10-24 14:21:49 +0200

    step3

---
+++ portfolio-site/step3/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // stampa i link di navigation.php
+
+    include_once "views/navigation.php";
+    $pageData = new stdClass();
+    $pageData->title = "Porfolio test-site";
+    $pageData->content = $nav;
+
+    require "templates/page.php";
+    echo $page;
+
+?>
---
+++ portfolio-site/step3/templates/page.php
@@ -0,0 +1,20 @@
+<?php
+
+    $page = "<!DOCTYPE html>
+
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>$pageData->title</title>
+    </head>
+    <body>
+
+        $pageData->content
+        
+    </body>
+    </html>
+    
+    ";
+
+?>
