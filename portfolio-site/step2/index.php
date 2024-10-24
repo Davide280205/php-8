ff2448acfc66155eba205171c6fe2e2cdddc6f8e
Davide <corpus.davide@gmail.com>
2024-10-24 14:24:02 +0200

    comments

--- portfolio-site/step2/index.php
+++ portfolio-site/step2/index.php
@@ -5,6 +5,7 @@
     include_once "views/navigation.php";
     $pageData = new stdClass();
     $pageData->title = "Porfolio test-site";
+    // mette la navigazione nel contenuto
     $pageData->content = $nav;
 
     require "templates/page.php";
--- portfolio-site/step3/index.php
+++ portfolio-site/step3/index.php
@@ -4,7 +4,8 @@
 
     include_once "views/navigation.php";
     $pageData = new stdClass();
-    $pageData->title = "Porfolio test-site";
+    $pageData->title = "Davide Corpus Porfolio";
+    // mette la navigazione nel contenuto
     $pageData->content = $nav;
 
     require "templates/page.php";
