f61aaa5d3796a1f1c1360f7084296504646a4b65
Davide <corpus.davide@gmail.com>
2024-10-24 13:59:48 +0200

    views - skills - projects

--- portfolio-site/step2/views/skills.php
+++ portfolio-site/step2/views/skills.php
@@ -1,12 +1,7 @@
 <?php
 
     $info = "<h1>Le mie competenze</h1>
-    <p>Lwggi tutte le mie competenze...</p>
-    
-
-    
-    
-    
+    <p>Leggi tutte le mie competenze...</p>     
     ";
 
 ?> 
