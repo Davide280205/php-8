671db13495d73cb2ddadd6710d7e6ea49c5a09cb
Davide <corpus.davide@gmail.com>
2025-01-13 10:09:05 +0100

    step-3

---
+++ portfolio-site-NEW/step-3/index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "views/navigation.php";
+    $pageData = new stdClass();
+    $pageData->title = "Nuovo sito portfolio";
+    $pageData->content = $nav;
+
+    require "templates/page.php";
+
+    echo $page;
+
+?>
