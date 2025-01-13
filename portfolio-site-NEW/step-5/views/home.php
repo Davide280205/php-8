ee3cb0c16fef2fdf89bfbde61bfd39fac6bd444a
Davide <corpus.davide@gmail.com>
2025-01-13 11:33:42 +0100

    step-6

---
+++ portfolio-site-NEW/step-5/views/home.php
@@ -0,0 +1,6 @@
+<?php
+
+$info = "<h1>Ciao, benvenuti nel mio sito personale</h1>
+<p>In questo sito troverete qualcosa...</p>";
+
+?>
---
+++ portfolio-site-NEW/step-6/views/navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+$nav = "
+
+<nav>
+<a href='index.php?page=skills'>Skills</a>
+<a href='index.php?page=projects'>Progetti</a>
+</nav>
+
+";
+?>
