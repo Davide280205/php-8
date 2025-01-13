90f5c1c74ee64aa9657af070a2cefc2f21762586
Davide <corpus.davide@gmail.com>
2025-01-13 09:26:02 +0100

    new files

--- portfolio-site-NEW/step-1/index.php
+++ portfolio-site-NEW/step-1/index.php
@@ -1,6 +1,11 @@
 <?php
 
     $pageData = new stdClass();
+    $pageData->title = "Nuovo sito portfolio";
+    $pageData->content = "<h1>Benvenuti!</h1>";
 
+    require "templates/page.php";
+
+    echo $page;
 
 ?>
