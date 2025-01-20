3cacf6ef4e8ba51476ab8a7746216fda8491287f
Davide280205 <corpus.davide@gmail.com>
2025-01-20 09:49:01 +0100

    quiz

--- quiz/step6/views/quiz.php
+++ quiz/step6/views/quiz.php
@@ -2,7 +2,25 @@
 
     $quizIsSubmitted = isset($_POST["quiz-submitted"]);
 
-    
+    if ($quizIsSubmitted) {
 
+        $answer = $_POST["answer"];
+        $info = showQuizResponse($answer);
+
+    }else{
+
+        include_once "views/quizform.php";
+
+    }
+
+    function showQuizResponse( string $answer ) : string{
+
+        $response = "<p>Hai cliccato $answer</p>";
+
+        $response .= "<p><a href='index.php?page=quiz'Vuoi riprovare?</a></p>";
+
+        return $response;
+
+    }
 
 ?>
