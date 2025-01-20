30661cfcaddbf5e9ddb9eeb5d3ea5b86dc8b36a6
Davide280205 <corpus.davide@gmail.com>
2025-01-20 09:22:58 +0100

    quizform

--- quiz/step5/views/quizform.php
+++ quiz/step5/views/quizform.php
@@ -1,5 +1,21 @@
 <?php
-$info= "<h1>Quiz</h1>
+$info= "
+
+<form method='post' action='index.php?page=quiz'>
+
+    <p>E' difficile o divertente imparare il PHP?</p>
+
+<select name='answer'>
+
+    <option value='yes'> Si è difficile </option>
+
+    <option value='yes'> No è divertente </option>
+
+</select>
+
+<input type='submit' name='quiz-submitted' value='post' />
+
+</form>
 
 ";
 ?>
