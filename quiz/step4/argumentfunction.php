a4d5fc450814587970a452331056c5b9c6bca515
Davide280205 <corpus.davide@gmail.com>
2025-01-20 08:49:47 +0100

    quiz - step4

---
+++ quiz/step4/argumentfunction.php
@@ -0,0 +1,17 @@
+<?php
+
+function getParagraph( string $content){
+
+    return "<p>$content</p>";
+
+}
+
+$output = getParagraph("voglio questo testo nel mio primo paragrafo");
+
+$output .= "<h1>titolo principale</h1>";
+
+$output .= getParagraph("... e quest'altro nell'ultimo paragrafo");
+
+echo $output;
+
+?>
---
+++ quiz/step4/testfunction.php
@@ -0,0 +1,12 @@
+<?php
+
+function getParagraph(){
+
+    echo "<p>paragrafo creato da una funzione</p>";
+
+}
+
+getParagraph();
+getParagraph();
+
+?>
