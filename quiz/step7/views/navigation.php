e80edeb48ec22b2a821a7825abc09018042f37b4
Davide280205 <corpus.davide@gmail.com>
2025-01-20 10:06:37 +0100

    step7

---
+++ quiz/step7/views/navigation.php
@@ -0,0 +1,8 @@
+<?php
+$nav= "
+<nav>
+    <a href='index.php?page=search'>Search on Bing</a>
+    <a href='index.php?page=quizform'>Dynamic quiz</a>
+</nav>
+";
+?>
---
+++ quiz/step7/views/search.php
@@ -0,0 +1,10 @@
+<?php
+$info= "<form method='get' action='https://www.bing.com/search/'>
+    
+    <input type='text' name='q'>
+
+    <input type='submit' value='search on Bing'>
+    
+    </form>
+";
+?>
