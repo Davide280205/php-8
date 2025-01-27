206f2fab2709ded1fb468fba6fdd04b5a78c2593
Davide280205 <corpus.davide@gmail.com>
2025-01-27 09:26:41 +0100

    step2

---
+++ files/step2/get_contents.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Files</title>
+</head>
+<body>
+
+    <?php nl2br(readfile('C:/private/sonnet.txt')); ?>
+    
+</body>
+</html>
