284f16f4d30b5ec700303d113e7d6e401d074559
Davide280205 <corpus.davide@gmail.com>
2025-01-29 10:04:41 +0100

    step7

---
+++ files/step7/fopen_write.php
@@ -0,0 +1,45 @@
+
+
+
+<?php
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Write Only</title>
+    <style>
+        label {
+            font-weight: bold;
+            display: inline-block;
+            float: left;
+            margin-right: 15px;
+        }
+        textarea {
+            width: 400px;
+            height: 125px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- il form -->
+
+<form name="writeFile" method="post" action="fopen_write.php">
+    <p>
+        <label for="contents">Write this to file:</label>
+        <textarea name="contents" id="contents"></textarea>
+    </p>
+    <p>
+        <input name="putContents" type="submit" value="Write to file">
+    </p>
+</form>
+
+
+</body>
+</html>
