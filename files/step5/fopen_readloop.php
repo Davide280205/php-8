d010a32d2cb896ba9b49b2b2b7f85f0b31828f22
Davide280205 <corpus.davide@gmail.com>
2025-01-29 09:17:05 +0100

    readloop

---
+++ files/step5/fopen_readloop.php
@@ -0,0 +1,20 @@
+<?php
+
+$filename = 'C:/private/sonnet.txt';
+
+$file = fopen($filename, 'r');
+
+$contents = '';
+
+while (feof($file)) {
+
+    $contents .= fgets($file);
+
+}
+
+// chiude il file fclose sta per file close
+fclose($file);
+
+echo nl2br($contents);
+
+?>
