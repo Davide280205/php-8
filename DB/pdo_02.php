feae2f94be30c1bd2691816cd3e036e001199061
Davide280205 <corpus.davide@gmail.com>
2025-02-10 10:05:08 +0100

    pdo02

---
+++ DB/pdo_02.php
@@ -0,0 +1,49 @@
+<?php
+
+    require_once 'includes/connection.php';
+    require_once 'includes/utility_funcs.php';
+    $conn = dbConnect('read');
+
+    // l'asterisco unisce tutto
+
+    $sql = 'SELECT * FROM images';
+
+    // esegue la query e salva il risultato nella variabile $result
+    $result = $conn->query($sql);
+
+    // nell'array mettere SEMPRE 2 perchè se c'è un errore è utile il terzo paragrafo
+    $error = $conn->errorInfo()[2];
+
+    if (!$error) {
+
+        $numRows = $result->rowCount();
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO</title>
+</head>
+<body>
+
+    <?php
+
+        if ($error){
+
+            echo "<p>$error</p>";
+
+        } else {
+
+            echo "<p>Nella tabella ci sono $numRows righe</p>";
+
+        }
+
+    ?>
+    
+</body>
+</html>
