06eee798487f45a7e0dade238bc78ce0e47495e6
Davide280205 <corpus.davide@gmail.com>
2025-02-19 09:10:26 +0100

    includes

---
+++ DB/CRUD/includes/utility_funcs.php
@@ -0,0 +1,17 @@
+<?php
+
+    function safe($text){
+        
+        return htmlspecialchars($text, double_encode: false);
+
+    }
+
+    // $testo = "Ciao <b>mondo</b>";
+
+    // echo $testo;
+
+        // la funzione htmlspecialchars serve a sanitizzare il codice
+
+    // echo htmlspecialchars($testo);
+
+?>
