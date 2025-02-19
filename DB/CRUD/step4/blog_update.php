61d3b468530e26cbdd5091517e91ad595ffd6856
Davide280205 <corpus.davide@gmail.com>
2025-02-19 11:55:53 +0100

    blog_update

--- DB/CRUD/step4/blog_update.php
+++ DB/CRUD/step4/blog_update.php
@@ -1,30 +1,58 @@
 <?php
 
-    require_once '../includes/connection.php';
-    require_once '../includes/utility_funcs.php';
+require_once '../includes/connection.php';
+require_once '../includes/utility_funcs.php';
 
-    $OK = false;
 
-    $conn = dbConnect('write');
+$OK = false;
 
-    if(isset($_GET['article_id']) && !$_POST){
+$conn = dbConnect('write');
 
-        // prepara una query SQL protetta con un placeholder
-        $sql = 'SELECT article_id, title, article FROM blog WHERE article_id = ?';
+if (isset($_GET['article_id']) && !$_POST){
 
-        $stmt = $conn->prepare($sql);
+    // prepara una query SQL protetta con un placeholder
+    $sql = 'SELECT article_id, title, article FROM blog WHERE article_id = ?';
 
-        $OK = $stmt->execute([$_GET['article_id']]);
+    $stmt = $conn->prepare($sql);
 
-        $stmt->bindColumn(1, $article_id);
-        $stmt->bindColumn(2, $title);
-        $stmt->bindColumn(3, $article);
+    $OK = $stmt->execute([$_GET['article_id']]);
 
-        $stmt->fetch();
 
-    }
+    $stmt->bindColumn(1, $article_id);
+    $stmt->bindColumn(2, $title);
+    $stmt->bindColumn(3, $article);
+
+    $stmt->fetch();
+
+
+
+}
+
+
+if (!isset($_GET['article_id'])){
+
+    $url = 'http://localhost/php-8/DB/CRUD/step4/blog_list.php';
+    header("Location: $url");
+    exit;
+
+
+}
+
+
+if (isset($stmt)){
+
+
+    $error = $stmt->errorInfo()[2];
+
+}
+
+
+
+
 
 ?>
+
+
 
 <!DOCTYPE HTML>
 <html>
@@ -37,18 +65,42 @@
 <body>
 <h1>Update Blog Entry</h1>
 <p><a href="blog_list.php">List all entries </a></p>
+
+<?php
+
+
+if (isset($error)){
+
+    echo "<p class='warning'>Error: $error </p>";
+
+}
+
+
+if ($article_id == 0) { ?>
+
+    <p class="warning">Richiesta non valida, il record non esiste</p>
+
+
+<?php } else { ?>
+
+
 <form method="post" action="blog_update.php">
     <p>
         <label for="title">Title:</label>
-        <input name="title" type="text" id="title">
+        <input name="title" type="text" id="title" value="<?= safe($title);?>">
     </p>
     <p>
         <label for="article">Article:</label>
-        <textarea name="article" id="article"></textarea>
+        <textarea name="article" id="article"><?= safe($article);?></textarea>
     </p>
     <p>
         <input type="submit" name="update" value="Update Entry">
+        <input name="article_id" type="hidden" value="<?= $article_id; ?>">
     </p>
 </form>
+
+
+<?php } ?>
+
 </body>
 </html>
