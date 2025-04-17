e51fa7e4743d7b69ad6645dff4732a27c61e535d
Davide280205 <corpus.davide@gmail.com>
2025-04-17 15:44:26 +0200

    wordpress

---
+++ DB/includes/connection.php
@@ -0,0 +1,38 @@
+<?php
+
+    function dbConnect($usertype){
+
+        $host = 'localhost';
+        $db = 'phpsol';
+
+        if ($usertype == 'read'){
+
+            $user = 'psread';
+            $pwd = 'lupo';
+
+    }elseif ($usertype == 'write'){
+
+            $user = 'pswrite';
+            $pwd = 'faina';
+
+    }else{
+
+        exit('Utente non riconosciuto');
+
+    }
+
+    // Connsessione al Database
+
+    try {
+
+        return new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+
+    } catch (PDOException $e){
+
+        echo $e->getMessage();
+
+    }
+
+    }
+
+?>
