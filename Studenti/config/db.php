2143b17bb131844c57a6db2bd5b81e3231f5420d
Davide280205 <corpus.davide@gmail.com>
2025-04-28 09:51:29 +0200

    db connect

---
+++ Studenti/config/db.php
@@ -0,0 +1,20 @@
+<!-- Questo codice ha il compito di connettersi al database mvc_studenti -->
+
+<?php
+
+$host = 'localhost';
+$db = 'mvc_studenti';
+$user = 'root';
+$pass = '';
+
+$conn = "mysql:host=$host;dbname=$db";
+
+try {
+
+    $pdo = new PDO($conn, $user, $pass);
+
+} catch (PDOException $e) {
+
+    die("Errore di connessione DB: " . $e->intl_get_error_message());
+
+}
