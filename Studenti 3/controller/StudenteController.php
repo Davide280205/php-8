672ea852d2e129965f1e6d900b7607b74aa8ecc3
Davide280205 <corpus.davide@gmail.com>
2025-05-15 14:28:51 +0200

    Studenti 3

---
+++ Studenti 3/controller/StudenteController.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ . '/../model/Studente.php';
+
+
+class StudenteController {
+
+	// variabile per il modello Studente
+	private $studenteModel;
+
+
+
+	public function __construct(){
+
+		// crea l'oggetto Studente 
+		$this->studenteModel = new Studente();
+
+	}
+
+// Metodo per visualizzare lista di tutti gli studenti
+	public function lista() {
+
+
+		// Recupera gli studenti dal modello
+		$studenti = $this->studenteModel->tutti();
+
+
+		// carica la vista della lista studenti
+		require __DIR__ . '/../view/lista_studenti.php';
+
+
+
+
+	}
+
+
+
+	public function dettaglio($id){
+
+		//recupero i dettagli dello studente dal modello
+		$studente = $this->studenteModel->trovaPerId($id);
+
+
+		require __DIR__ . '/../view/dettaglio_studente.php';
+
+	}
+
+
+	public function loadForm(){
+
+		require __DIR__ . "/../view/aggiungi_studente.php";
+
+	}
+
+
+	public function store(){
+
+
+			if (isset($_POST['nome'], $_POST['cognome'], $_POST['email'], $_POST['telefono'] )){
+
+				$this->studenteModel->nuovo($_POST['nome'], $_POST['cognome'], $_POST['email'], $_POST['telefono']);
+			}
+
+			header("Location: index.php");
+
+
+	}
+
+
+
+
+
+}
---
+++ Studenti 3/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+require_once __DIR__ . '/controller/StudenteController.php';
+
+$controller = new StudenteController();
+
+
+
+$action = $_GET['action'] ?? 'lista';
+$id = $_GET['id'] ?? null;
+
+
+
+if ($action ==='dettaglio' && $id) {
+
+	$controller->dettaglio($id);
+
+} elseif($action==='loadForm'){
+
+	$controller->loadForm();
+
+}elseif($action==='store'){
+
+	$controller->store();
+
+}else{
+
+	$controller->lista();
+
+}
